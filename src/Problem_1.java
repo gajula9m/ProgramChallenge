ddf81693a487e0a4d297f2f954fbddcfa5456b08
Mehul <gajula9m@gmail.com>
2022-02-09 21:16:40 -0500

    Updated Problem 1 to pass numerous test cases

--- src/Problem_1.java
+++ src/Problem_1.java
@@ -1,227 +1,186 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Problem_1 {
 	
-	private final static int NO_LIGHT = 0; // no light in room
-	private final static int LIGHT = 1; // light in room but can't reach
-	private final static int LIGHT_AND_ACCESS = 2; // light in room and can reach
+	static ArrayList<Room>[][] switches;
+	static boolean[][] lightOn;
+	static boolean[][] roomVisited;
 	
-	// a 2D array list of int[] that store the type of room and the switches locatedd in that room
-	private static ArrayList<int[]>[][] barn; 
-	
-	/* main function */
-	public static void main(String[] args) throws IOException{
-		
+	/* 
+	 * main function
+	 */
+	public static void main (String[] args) throws IOException {
 		Scanner scan = new Scanner(System.in);
 		
+		//input file location
 		System.out.println("Enter input file: ");
-		String in_file = scan.nextLine();
+		String inFile = scan.nextLine();
 		
-		readFile(in_file);
+		// read File
+		readFile(inFile);
 		
-		int startX = 0;
-		int startY = 0;
+		// the top left room is already on, and that is our starting point
+		lightOn[0][0] = true;
+		navigate(0,0);
 		
-		navigateBarn(startX, startY);
+		// number of lights on
+		int lightsOn = countLights();
 		
-		int lights = countLights();
-		
+		// the output file location
 		System.out.println("Enter output file: ");
 		String outFile = scan.nextLine();
 		
-		writeFile(lights, outFile);
+		
+		// write number of lights on to output file
+		writeFile(outFile, lightsOn);
 	}
 	
 	/*
-	 * counts the total number of lights that have been turned on
+	 * writes the information to the output file
 	 */
-	private static int countLights() {
-		int lights = 0;
-		
-		for (ArrayList<int[]>[] row : barn) {
-			for (ArrayList<int[]> arr : row) {
-				if (arr.get(0)[0] == 2 || arr.get(0)[0] == 1) {
-					lights++;
-				}
-			}
-		}
-		
-		return lights;
-	}
-	
-	/*
-	 * Once a new LIGHT_AND_ACCESS room is created, it may be possible that this new room starts up 
-	 * a new path, connecting the path to rooms that have been lit and not connected to a path. 
-	 * This function will check if those paths have been created, and will reassign certain rooms 
-	 * with just LIGHT to LIGH_AND_ACCESSS, recursively calling the navigateBarn method.
-	 */
-	private static void checkNeighbors(int roomX, int roomY) {
-		
-		int left = roomX - 1;
-		int right = roomX + 1;
-		int top = roomY - 1;
-		int bottom = roomY + 1;
-		
-		
-		// check if left already has a value of LIGHT, and if so, make it LIGHT_AND_ACCESS
-		if (left >= 0) {
-			if (barn[left][roomY].get(0)[0] == LIGHT) {
-				barn[left][roomY].set(0, new int[] {LIGHT_AND_ACCESS});
-				navigateBarn(left, roomY);
-			}
-		}
-		
-		// check if right already has a value of LIGHT, and if so, make it LIGHT_AND_ACCESS
-		if (right < barn.length) {
-			if (barn[right][roomY].get(0)[0] == LIGHT) {
-				barn[right][roomY].set(0, new int[] {LIGHT_AND_ACCESS});
-				navigateBarn(right, roomY);
-			}
-		}
-		
-		// check if top already has a value of LIGHT, and if so, make it LIGHT_AND_ACCESS
-		if (top >= 0) {
-			if (barn[roomX][top].get(0)[0] == LIGHT) {
-				barn[roomX][top].set(0, new int[] {LIGHT_AND_ACCESS});
-				navigateBarn(roomX, top);
-			}
-		}
-		
-		// check if bottom already has a value of LIGHT, and if so, make it LIGHT_AND_ACCESS
-		if (bottom < barn.length) {
-			if (barn[roomX][bottom].get(0)[0] == LIGHT) {
-				barn[roomX][bottom].set(0, new int[] {LIGHT_AND_ACCESS});
-				navigateBarn(roomX, bottom);
-			}
-		}
-	}
-	
-	/*
-	 * Navigation starts from [0,0] in the array (or 1 ,1 in the input file).
-	 * Each switch in the room will be turned on and then if the room is right next to a room
-	 * that connects to a path, then that room's switches will all be activated by 
-	 * recursively calling the function
-	 */
-	private static void navigateBarn(int roomX, int roomY) {
-		for (int[] switches : barn[roomX][roomY]) {
-			/*
-			 *  make sure switches are actually switches and not
-			 *  the value of the room itself
-			 */
-			if (switches.length == 2) { 
-				/*
-				 * ignore if the switch is already on
-				 */
-				if (barn[switches[0]][switches[1]].get(0)[0] == LIGHT_AND_ACCESS) {
-					continue;
-				} else {
-					/* 
-					 * turn on switch
-					 */
-					barn[switches[0]][switches[1]].set(0, new int[] {LIGHT});
-					
-					/*
-					 * if connected to path, make it LIGHT_AND ACCESSIBLE SWITCH
-					 * and check that lights switches
-					 */
-					if (pathAvailable(switches[0], switches[1])) {
-						barn[switches[0]][switches[1]].set(0, new int[] {LIGHT_AND_ACCESS});
-						checkNeighbors(switches[0], switches[1]);
-						navigateBarn(switches[0], switches[1]);
-					}
-				}
-			}
-		}
-		
-		
-	}
-		
-	/*
-	 * Checks if their is a path that is next to a certain room, so that the current room
-	 * become a part of that path
-	 */
-	private static boolean pathAvailable(int roomX, int roomY) {
-		
-		int left = roomX - 1;
-		int right = roomX + 1;
-		int top = roomY - 1;
-		int bottom = roomY + 1;
-		
-		if (left >= 0) {
-			if (barn[left][roomY].get(0)[0] == LIGHT_AND_ACCESS) {
-				return true;
-			}
-		}
-		
-		if (right < barn.length) {
-			if (barn[right][roomY].get(0)[0] == LIGHT_AND_ACCESS) {
-				return true;
-			}
-		}
-		
-		if (top >= 0) {
-			if (barn[roomX][top].get(0)[0] == LIGHT_AND_ACCESS) {
-				return true;
-			}
-		}
-		
-		if (bottom < barn.length) {
-			if (barn[roomX][bottom].get(0)[0] == LIGHT_AND_ACCESS) {
-				return true;
-			}
-		}
-		
-		
-		return false;
-	}
-	
-	
-	/* 
-	 * Reads the input file and creates the appropriate sized barn array
-	 * Then enters all information about the switches in each room
-	 */
-	private static void readFile(String in_file) throws IOException {
-		BufferedReader bfr = new BufferedReader(new FileReader(new File(in_file)));
-		String line = bfr.readLine();
-		
-		String[] values = line.split(" ");
-		int gridSize = Integer.parseInt(values[0]);
-		int numLines = Integer.parseInt(values[1]);
-		
-		barn = new ArrayList[gridSize][gridSize];
-		
-		for (int i = 0; i < gridSize; i++) {
-			for (int j = 0; j < gridSize; j++) {
-				barn[i][j] = new ArrayList<int[]>();
-				barn[i][j].add(new int[] {NO_LIGHT});
-			}
-		}
-		
-		barn[0][0].set(0, new int[] {LIGHT_AND_ACCESS});
-		
-		
-		for (int i = 0; i < numLines; i++) {
-			line = bfr.readLine();
-			values = line.split(" ");
-			int switchRoomX = Integer.parseInt(values[0]) - 1;
-			int switchRoomY = Integer.parseInt(values[1]) - 1;
-			int lightRoomX = Integer.parseInt(values[2]) - 1;
-			int lightRoomY = Integer.parseInt(values[3]) - 1;
-			
-			barn[switchRoomX][switchRoomY].add(new int[] {lightRoomX, lightRoomY});
-		}
-		
-	}
-
-	/*
-	 * Writes the information to the designated output file
-	 */
-	private static void writeFile(int lightsOn ,String outFile) throws IOException {
-		PrintWriter pw = new PrintWriter(new FileOutputStream(new File(outFile)));
+	private static void writeFile(String writeFile, int lightsOn) throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter(new FileOutputStream(new File(writeFile)));
 		pw.println(lightsOn);
 		pw.close();
 	}
-}
+	
+	/*
+	 * uses the lightsOn array and counts the number of lights that
+	 * are switched on
+	 */
+	private static int countLights() {
+		
+		int lightsOn = 0;
+		
+		for (boolean[] row : lightOn) {
+			for (boolean light: row) {
+				if (light) {
+					lightsOn++;
+				} 	
+			}
+		}
+		
+		return lightsOn;
+	}
+	
+	/*
+	 * navigates through the barn, and while navigating, turns on the switches that
+	 * are located in the room. And then recursively, if possible, goes to that room 
+	 * turns on those lights
+	 */
+	private static void navigate(int roomX, int roomY) {
+		/*
+		 * saves time by not revisiting rooms where switches were turned on
+		 */
+		if (!roomVisited[roomX][roomY]) {
+			// set room visited to true
+			roomVisited[roomX][roomY] = true;
+			for (Room lightSwitch: switches[roomX][roomY]) {
+				if (!lightOn[lightSwitch.x][lightSwitch.y]) {
+					//turn on light if not on
+					lightOn[lightSwitch.x][lightSwitch.y] = true;
+					//check if light switch can be accessed 
+					if (canBeReached(lightSwitch.x, lightSwitch.y)) {
+						// if the room is accessible, then, navigate that room
+						navigate(lightSwitch.x, lightSwitch.y);
+					}
+				}
+			}
+			
+			// if neighboring rooms are turned on, then navigate those rooms too.
+			if (lightIsOn(roomX + 1, roomY)) {
+				navigate(roomX + 1, roomY);
+			}
+			if (lightIsOn(roomX - 1, roomY)) {
+				navigate(roomX - 1, roomY);
+			} 
+			if (lightIsOn(roomX, roomY + 1)) {
+				navigate(roomX, roomY + 1);
+			} 
+			if (lightIsOn(roomX, roomY - 1)) {
+				navigate(roomX, roomY - 1);;
+			}
+		}
+		
+	}
+	
+	/*
+	 *  Checks if a specific rooms lights are on
+	 */
+	public static boolean lightIsOn(int roomX, int roomY) {
+		return roomX >= 0 && roomX < lightOn.length && roomY >= 0 && roomY < lightOn[roomX].length 
+				&& lightOn[roomX][roomY];
+	}
+	
+	/*
+	 *  Checks if a specific room has been visited already
+	 */
+	public static boolean roomVisited(int roomX, int roomY) {
+		return roomX >= 0 && roomX < roomVisited.length && roomY >= 0 && roomY < roomVisited[roomX].length 
+				&& roomVisited[roomX][roomY];
+	}
+	
+	/*
+	 *  Checks if a room has neighboring rooms with lightsOn and has been visited.
+	 *  If so, then the room can be reached and returns true.
+	 */
+	private static boolean canBeReached(int roomX, int roomY) {
+		if (lightIsOn(roomX + 1, roomY) && roomVisited(roomX + 1, roomY)) {
+			return true;
+		} else if (lightIsOn(roomX - 1, roomY) && roomVisited(roomX - 1, roomY)) {
+			return true;
+		} else if (lightIsOn(roomX, roomY + 1) && roomVisited(roomX, roomY + 1)) {
+			return true;
+		} else if (lightIsOn(roomX, roomY - 1) && roomVisited(roomX , roomY - 1)) {
+			return true;
+		}
+		
+		return false;
+	}
+	/*
+	 * reads the file and declares the switches ArrayList and the lightsOn and
+	 * roomVisited boolean arrays with the given grid Size. Then reads through 
+	 * all the lines and assigns each room with the switches located in that room
+	 */
+	private static void readFile(String inFile) throws IOException {
+		BufferedReader bfr = new BufferedReader(new FileReader(inFile));
+		
+		String[] values = bfr.readLine().split(" ");
+		int gridSize = Integer.parseInt(values[0]);
+		int numLines = Integer.parseInt(values[1]);
+		
+		switches = new ArrayList[gridSize][gridSize];
+		lightOn = new boolean[gridSize][gridSize];
+		roomVisited = new boolean[gridSize][gridSize];
+		
+		for (int i = 0; i < gridSize; i++) {
+			for (int j = 0; j < gridSize; j++) {
+				// each room consists of switches that access other Rooms
+				switches[i][j] = new ArrayList<Room>();
+			}
+		}
+		
+		for (int i = 0; i < numLines; i++) {
+			values = bfr.readLine().split(" ");
+			int roomX = Integer.parseInt(values[0]) - 1;
+			int roomY = Integer.parseInt(values[1]) - 1;
+			int switchX = Integer.parseInt(values[2]) - 1;
+			int switchY = Integer.parseInt(values[3]) - 1;
+			switches[roomX][roomY].add(new Room(switchX,switchY));
+		}
+		bfr.close();
+	}
+	
+	/*
+	 * a class that stores the coordinates for the room
+	 */
+	static class Room {
+		public int x,y;
+		public Room(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 
+}
